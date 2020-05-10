8c8c0d28ea4d9ba31bc7c7694e96f1d62af0a4e4
小野和論 <matopenkw@gmail.com>
2020-05-11 02:10:14 +0900

    #4 Update main.go CORS対応

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module Carfare
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/gin v1.6.3
+	gopkg.in/ini.v1 v1.56.0
+)
--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"Carfare/handler"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/ini.v1"
 )
+
+const CONFIG_PATH = "pkg/conf/config.conf"
 
 func main() {
 	router := gin.Default()
+
+	// CORS
+	c, _ := ini.Load(CONFIG_PATH)
+	corsConfig := c.Section("CORS")
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{corsConfig.Key("approvalServerUrl").String()}
+	router.Use(cors.New(config))
 
 	router.GET("/getCarfare", handler.GetCarfare)
 
